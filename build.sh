fa58e3cd6d8e1d534284cad28483df29751aa42b
elrot <kamil@fogger.io>
2016-12-04 02:09:04 +0100

    remove 386

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ VERSIONS=("edge")
 
 # supported architectures map 
 # "APK_ARCH; QEMU_ARCH; TAG_ARCH"
-ARCHS=("x86;i386;386" "x86_64;x86_64;amd64" "armhf;arm;arm" "aarch64;aarch64;arm64")
+ARCHS=("x86_64;x86_64;amd64" "armhf;arm;arm" "aarch64;aarch64;arm64")
 
 IMAGE="fogger/alpine"
 MIRROR=${MIRROR:-http://dl-cdn.alpinelinux.org/alpine}
