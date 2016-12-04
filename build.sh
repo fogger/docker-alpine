003258d1bad38165662e048403161f91b5f57dc4
elrot <kamil@fogger.io>
2016-12-04 03:10:07 +0100

    manifest

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ VERSIONS=("edge")
 
 # supported architectures map 
 # "APK_ARCH; QEMU_ARCH; TAG_ARCH"
-ARCHS=("x86_64;x86_64;amd64" "armhf;arm;arm" "aarch64;aarch64;arm64")
+ARCHS=("x86_64;x86_64;amd64" "aarch64;aarch64;arm64" "armhf;arm;arm")
 
 IMAGE="fogger/alpine"
 MIRROR=${MIRROR:-http://dl-cdn.alpinelinux.org/alpine}
